951cd64767d613e38c1841ee6cf0f0c0c7618f85
Philemon-ndayishimiye <philemonndayi@gmail.com>
2026-04-13 18:38:56 +0200

    cors

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ const app = express();
 // Middlewares
 // ========================
 
-// CREATE + UPLOAD (uses your service)
+// CREATE + UPLOAD (uses on upload service)
 app.post(
   "/application-docs",
   upload.single("documentUrl"), //  matches frontend
@@ -41,7 +41,13 @@ app.post(
   },
 );
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
